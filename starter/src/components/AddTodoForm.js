e1672867df6b9add175cad0c40dede242c99ea84
netocruz <galaxy.net0710@gmail.com>
2022-09-12 16:01:02 -0500

    Api call with redux

--- starter/src/components/AddTodoForm.js
+++ starter/src/components/AddTodoForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { addTodo } from '../redux/todoSlice';
+import { addTodoAsync } from '../redux/todoSlice';
 
 const AddTodoForm = () => {
 	const [value, setValue] = useState('');
@@ -8,12 +8,12 @@ const AddTodoForm = () => {
 
 	const onSubmit = (event) => {
 		event.preventDefault();
-		if(value){
-          dispatch(
-			addTodo({
-				title:value,
-			})
-		  )
+		if (value) {
+			dispatch(
+				addTodoAsync({
+					title: value,
+				})
+			);
 		}
 	};
 
